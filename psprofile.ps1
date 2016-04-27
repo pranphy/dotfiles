f4060e6777bf5f37d1e1b86902a5ff4d812d4333
Prakash Gautam <pranphy@gmail.com>
2016-04-27 12:36:14 +0545

    Add powershell config

---
+++ psprofile.ps1
@@ -0,0 +1,12 @@
+# Powershell v.1 profile
+#
+
+# Trying to make my powershell Bashlike :D
+new-alias grep findstr
+
+
+# To make links (symbolic )
+function mklink { cmd /c mklink $args }
+
+# I am already improved 
+new-alias vi vim
