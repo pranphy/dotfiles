4cf5c0e1f026cf18d5bc144da334c6bcbc250cef
Prakash Gautam <pranphy@gmail.com>
2021-02-09 17:09:31 -0500

    Organize path stuffs around a bit

--- bash/Praaksh_env.sh
+++ bash/Praaksh_env.sh
@@ -7,12 +7,16 @@
 
 if [[ ${HOSTNAME} == "Prakash" ]]
 then
+    export CDPATH=.:~:~/GitRepos:~/Sabthok/Education/Graduate/Drexel:~/Sabthok/Programming/Projects:~/.Rough
     export RCLONE_HOME="/opt/rclone"
     export PATH="$RCLONE_HOME:${PATH}"
 
     #Zotero home
     export ZOTERO_HOME="/opt/Zotero"
     export PATH="$ZOTERO_HOME:${PATH}"
+
+    LAMPP=/opt/lampp
+    export PATH=${LAMPP}/bin:${PATH}
 
 
     export PATH="${HOME}/.local/bin:$PATH"
@@ -21,5 +25,15 @@ then
         GIT_PROMPT_ONLY_IN_REPO=1
         source ${HOME}/.Rough/GitPrompt/gitprompt.sh
     fi
+
+    if type rg &> /dev/null; then
+        export FZF_DEFAULT_COMMAND='rg --ignore-file ~/.rgignore --files --hidden'
+    fi
+    export FZF_DEFAULT_OPTS='--height 60% --layout=reverse --border'
+
     export MYROOT="${HOME}/MyRoot"
+    if [[ ${MYROOT} != "" ]]
+    then
+        source ${MYROOT}/enable.sh
+    fi
 fi
