66049fb89ce8bf5e6300d268ca8ed8410b34416a
Prakash Gautam <pranphy@gmail.com>
2021-12-26 03:20:31 -0500

    Move treesitter config to lua.

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,11 +1,9 @@
-" vim: ft=lua
-lua <<EOF
-require'nvim-treesitter.configs'.setup {
+-- vim: ft=lua
+require('nvim-treesitter.configs').setup({
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   --additional_vim_regex_highlighting = true,
   highlight = {
     enable = true,              -- false will disable the whole extension
     disable = { "latex","tex"},  -- list of language that will be disabled
   },
-}
-EOF
+})
