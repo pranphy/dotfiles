6ad52a58a5031f08012e4cb6b21259eed35f2ea0
Prakash Gautam <pranphy@gmail.com>
2022-11-15 01:18:53 -0500

    Add keymap for lsp and enable / and ? completion

--- nvim/plugin-config/cmp-nvim.lua
+++ nvim/plugin-config/cmp-nvim.lua
@@ -179,10 +179,10 @@ cmp.setup.filetype('gitcommit', {
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline({ '/', '?' }, {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = { { name = 'buffer' } }
--- })
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = { { name = 'buffer' } }
+})
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
     -- mapping = cmp.mapping.preset.cmdline(),
