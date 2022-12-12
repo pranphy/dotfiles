59edc85299ff37e824f6223842e274f12195436d
Prakash <fku2vr@virginia.edu>
2022-12-11 23:48:46 -0500

    Add bunch of new plugins.

--- nvim/lua/core/plugin.lua
+++ nvim/lua/core/plugin.lua
@@ -1,28 +1,32 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 -- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'neovim/nvim-lspconfig'
 
-  -- Post-install/update hook with neovim command
-  use {'nvim-lua/plenary.nvim'}
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', }
+  use { 'nvim-telescope/telescope.nvim' }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use { "nvim-telescope/telescope-bibtex.nvim",
     config = function () require"telescope".load_extension("bibtex") end,
   }
 
+
   -- Use dependency and run lua function after load
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
-  }
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }, }
+  use 'nvim-tree/nvim-web-devicons'
+  use 'lewis6991/gitsigns.nvim'
+  use 'TimUntersberger/neogit'
+  use { 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true }, }
   use { 'nvim-tree/nvim-tree.lua', tag = 'nightly', requires = { 'nvim-tree/nvim-web-devicons', }, }
   use "L3MON4D3/LuaSnip"
   use 'lervag/vimtex'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 
+  use 'renerocksai/telekasten.nvim'
 end)
--- nvim/lua/plugin/init.lua
+++ nvim/lua/plugin/init.lua
@@ -1,7 +1,12 @@
 require("plugin.lualine")
+require("plugin.nvim-lsp")
 require("plugin.nvim-tree")
 require("plugin.treesitter")
 require("plugin.telescope")
 require("plugin.luasnip")
 require("plugin.vimtex")
+require("plugin.cmp-nvim")
+require("plugin.telekasten")
+require("plugin.gitsigns")
+require("plugin.neogit")
 
