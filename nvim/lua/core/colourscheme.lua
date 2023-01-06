059bc8f3dbcceae404ca83c70c9ae34794f178dc
Prakash <fku2vr@virginia.edu>
2023-01-06 17:32:40 -0500

    Reformat colourscheme configuration

--- nvim/lua/core/colourscheme.lua
+++ nvim/lua/core/colourscheme.lua
@@ -1,43 +1,75 @@
-local dracula = require("dracula")
-dracula.setup({
-  -- customize dracula color palette
-  colors = {
-    -- bg = "#282A36",
-    -- fg = "#F8F8F2",
-    selection = "#44475A",
-    comment = "#6272A4",
-    red = "#FF5555",
-    orange = "#FFB86C",
-    yellow = "#F1FA8C",
-    green = "#50fa7b",
-    purple = "#BD93F9",
-    cyan = "#8BE9FD",
-    pink = "#FF79C6",
-    bright_red = "#FF6E6E",
-    bright_green = "#69FF94",
-    bright_yellow = "#FFFFA5",
-    bright_blue = "#D6ACFF",
-    bright_magenta = "#FF92DF",
-    bright_cyan = "#A4FFFF",
-    bright_white = "#FFFFFF",
-    menu = "#21222C",
-    visual = "#3E4452",
-    gutter_fg = "#4B5263",
-    nontext = "#3B4048",
+local config = {
+  dracula = {
+    -- customize dracula color palette
+    colors = {
+      -- bg = "#282A36",
+      -- fg = "#F8F8F2",
+      selection = "#44475A",
+      comment = "#6272A4",
+      red = "#FF5555",
+      orange = "#FFB86C",
+      yellow = "#F1FA8C",
+      green = "#50fa7b",
+      purple = "#BD93F9",
+      cyan = "#8BE9FD",
+      pink = "#FF79C6",
+      bright_red = "#FF6E6E",
+      bright_green = "#69FF94",
+      bright_yellow = "#FFFFA5",
+      bright_blue = "#D6ACFF",
+      bright_magenta = "#FF92DF",
+      bright_cyan = "#A4FFFF",
+      bright_white = "#FFFFFF",
+      menu = "#21222C",
+      visual = "#3E4452",
+      gutter_fg = "#4B5263",
+      nontext = "#3B4048",
+    },
+    -- show the '~' characters after the end of buffers
+    show_end_of_buffer = true, -- default false
+    -- use transparent background
+    transparent_bg = true, -- default false
+    -- set custom lualine background color
+    lualine_bg_color = "#44475a", -- default nil
+    -- set italic comment
+    italic_comment = true, -- default false
+    -- overrides the default highlights see `:h synIDattr`
+    overrides = {
+      -- Examples
+      -- NonText = { fg = dracula.colors().white }, -- set NonText fg to white
+      -- NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
+      -- Nothing = {} -- clear highlight of Nothing
+    },
   },
-  -- show the '~' characters after the end of buffers
-  show_end_of_buffer = true, -- default false
-  -- use transparent background
-  transparent_bg = true, -- default false
-  -- set custom lualine background color
-  lualine_bg_color = "#44475a", -- default nil
-  -- set italic comment
-  italic_comment = true, -- default false
-  -- overrides the default highlights see `:h synIDattr`
-  overrides = {
-    -- Examples
-    -- NonText = { fg = dracula.colors().white }, -- set NonText fg to white
-    -- NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
-    -- Nothing = {} -- clear highlight of Nothing
-  },
-})
+
+  -- setup must be called before loading the colorscheme
+  -- Default options:
+  gruvbox = {
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = true,
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  }
+}
+
+local colourscheme = "gruvbox"
+require(colourscheme).setup(config[colourscheme])
+
+vim.cmd("colorscheme "..colourscheme)
+vim.cmd[[
+highlight SignColumn guibg=NONE
+highlight Normal guibg=NONE ctermbg=NONE
+highlight VertSplit guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE
+]]
+
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -25,10 +25,3 @@ set.cmdheight = 0
 vim.g.mapleader = ' '
 -- vim.g.colors_name = 'gruvbox'
 
-vim.cmd[[
-colorscheme dracula
-highlight SignColumn guibg=NONE
-highlight Normal guibg=NONE ctermbg=NONE
-highlight VertSplit guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE
-]]
-
