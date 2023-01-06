305aae9fa5cef4bad2475832f7b7a8593fc74c3f
Prakash <fku2vr@virginia.edu>
2023-01-06 17:37:44 -0500

    Make nepali mapping usable

--- nvim/lua/plugin/luasnip.lua
+++ nvim/lua/plugin/luasnip.lua
@@ -10,8 +10,8 @@ ls.config.set_config({
 })
 
 vim.cmd([[
-imap <silent><expr> df luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : 'df' 
-inoremap <silent> fd <cmd>lua require'luasnip'.jump(-1)<Cr>
+imap <silent><expr> <A-space> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<A-space>'
+inoremap <silent> <A-shift-space> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
 snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
 snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
