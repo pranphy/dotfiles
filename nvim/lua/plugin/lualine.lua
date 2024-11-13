ef0eb93d66782173a9d498a8add34b9c7994b03b
Prakash Gautam <pranphy@gmail.com>
2024-11-13 10:31:29 -0500

    Add current time to lualine

--- nvim/lua/plugin/lualine.lua
+++ nvim/lua/plugin/lualine.lua
@@ -1,3 +1,7 @@
+local function curtime()
+    return vim.fn.strftime("%H:%M")
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -21,7 +25,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {{'filename',path=1}},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'encoding', 'fileformat', 'filetype',curtime},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
