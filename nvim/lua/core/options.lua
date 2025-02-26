6cde60d67ac6af4bf64f8daa80937be84905c4ac
Prakash Gautam <pranphy@gmail.com>
2025-02-25 23:24:15 -0500

    Use vim.o instead of vim.opt

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,24 +1,25 @@
-vim.opt.hidden = true
+vim.o.hidden = true
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.wrap = false
-vim.opt.breakindent = true
-vim.opt.conceallevel = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.wrap = false
+vim.o.breakindent = true
+vim.o.conceallevel = 2
+--vim.o.clipboard:append { 'unnamed' }
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.fileformat = 'unix'
-vim.opt.foldmethod  = 'syntax'
-vim.opt.hlsearch = false
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.fileformat = 'unix'
+vim.o.foldmethod  = 'syntax'
+vim.o.hlsearch = false
 
-vim.opt.inccommand = 'split'
-vim.opt.laststatus = 3
-vim.opt.cmdheight = 0
+vim.o.inccommand = 'split'
+vim.o.laststatus = 3
+vim.o.cmdheight = 0
 
 vim.g.mapleader = ' '
 -- vim.g.colors_name = 'gruvbox'
